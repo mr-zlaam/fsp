95c0fddc74a5ded31599a26183439daaa05ba32d
zlaam <zlaam.dev@gmail.com>
2025-01-06 11:34:14 +0500

    feat: Add dashboard

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 function RootLayout() {
   return (
     <>
-      hero
       <Outlet />
     </>
   );
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,11 @@
+import Dashboard from "@/pageComponents/dashboard/dashboard";
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <React.Fragment>
+      < Dashboard />
+    </React.Fragment>
   )
 }
 
